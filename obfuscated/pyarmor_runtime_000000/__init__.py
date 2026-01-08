15c164f7f3c6b17d896303862faf4110291a5dbe
Liu <2154335573@qq.com>
2026-01-08 11:21:57 +0800

    更新 obfuscated 运行时文件和依赖要求

--- obfuscated/pyarmor_runtime_000000/__init__.py
+++ obfuscated/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,10 @@
 # Pyarmor 9.2.3 (trial), 000000, 2026-01-08T11:03:39.793817
-from .pyarmor_runtime import __pyarmor__
+import sys
+import os
+
+# 确保当前目录在模块搜索路径中
+current_dir = os.path.dirname(__file__)
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+from pyarmor_runtime import __pyarmor__
